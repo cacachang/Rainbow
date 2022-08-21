6c0c89d1728708cc909494c04de120ce4700f0b6
Ning <a24701770@gmail.com>
2022-08-21 12:36:54 +0800

    add test & server & gitignore

---
+++ test/application_test.rb
@@ -0,0 +1,16 @@
+require_relative "test_helper"
+  class TestApp < Rainbow::Application
+end
+
+class RainbowAppTest < Minitest::Test
+  include Rack::Test::Methods
+  def app
+  TestApp.new
+end
+  def test_request
+    get "/"
+    assert last_response.ok?
+    body = last_response.body
+    assert body["Hello"]
+  end 
+end
